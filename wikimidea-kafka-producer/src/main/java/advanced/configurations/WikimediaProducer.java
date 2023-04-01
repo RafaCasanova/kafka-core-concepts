d4d0542bfe64496b43338e05fe203b3cfa2208ee
Rafael Casanova <rafa.casanova.matos@gmail.com>
2023-04-01 13:04:41 -0300

    configurações de seguraça, e compressão de dados!

--- wikimidea-kafka-producer/src/main/java/advanced/configurations/WikimediaProducer.java
+++ wikimidea-kafka-producer/src/main/java/advanced/configurations/WikimediaProducer.java
@@ -1,15 +1,13 @@
 package advanced.configurations;
 
 import com.launchdarkly.eventsource.EventSource;
-import org.apache.kafka.clients.producer.Callback;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.net.URI;
 import java.util.Objects;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 public class WikimediaProducer {
     public static void main(String[] args) throws InterruptedException {
@@ -18,9 +16,22 @@ public class WikimediaProducer {
         String bootstrapserver = "localhost:19092,localhost:19093,localhost:19094";
 
         Properties properties = new Properties();
-        properties.setProperty("bootstrap.servers",bootstrapserver);
-        properties.setProperty("key.serializer", StringSerializer.class.getName());
-        properties.setProperty("value.serializer",StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,bootstrapserver);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+
+        //configurações de seguraça, geralmente vem pro padrão na verções atuais(verções 3+)
+        properties.setProperty(ProducerConfig.ACKS_CONFIG,"-1");// -1 = all
+        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG,"true");
+        properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION,"5");
+        properties.setProperty(ProducerConfig.RETRIES_CONFIG,Integer.toString(Integer.MAX_VALUE));
+
+        //compressão e tamanhado de dados maiores
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG,"20");//delay de 20 milissegundos
+        //aumentando o tamanho de por padrão e 16 kbytes, estamos colocando para 32 kbytes
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG,Integer.toString(32*1024));//obs: numero total em bytes
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG,"snappy");
+
         KafkaProducer<String,String> producer = new KafkaProducer<>(properties);
 
 
